8d0a7695ac7a026e083c85143032db0abd7fedef
Clement Sam <clementsam75@gmail.com>
2025-03-17 02:23:23 +0000

    fix payment param_list field

--- examples/payment/main.go
+++ examples/payment/main.go
@@ -49,7 +49,7 @@ func main() {
 					DebitAccountType:    "Corporate",
 					CreditAccountBranch: "Accra",
 					CreditAccountType:   "Corporate",
-					Amount:              "10",
+					Amount:              decimal.NewFromInt(10),
 					Currency:            "GHS",
 				}),
 				Amount:   decimal.NewFromInt(10),
@@ -68,7 +68,7 @@ func main() {
 					SenderName:             "Freeman Kay",
 					Currency:               "GHS",
 					SenderMobileNo:         "0202205113",
-					Amount:                 "40",
+					Amount:                 decimal.NewFromInt(40),
 					SenderID:               "QWE345Y4",
 					BeneficiaryName:        "Stephen Kojo",
 					BeneficiaryMobileNo:    "0233445566",
@@ -90,7 +90,7 @@ func main() {
 					BeneficiaryName:      "Owen",
 					BeneficiaryPhone:     "233543837123",
 					TransferReferenceNo:  "QWE345Y4",
-					Amount:               "10",
+					Amount:               decimal.NewFromInt(10),
 					Currency:             "GHS",
 					TransferType:         "spot",
 				}),
@@ -108,7 +108,7 @@ func main() {
 					CustomerName:  "Freeman Kay",
 					CustomerRefNo: "239729",
 					ProductCode:   "PassBio",
-					FormDataValue: []ecobank.BillPaymentField{
+					FormDataValue: ecobank.FormDataArray{
 						{FieldName: "LastName", FieldValue: "Kojo"},
 						{FieldName: "FirstName", FieldValue: "Kwame"},
 						{FieldName: "Amount", FieldValue: "300"},
@@ -131,7 +131,7 @@ func main() {
 					CustomerName:  "Owen Kay",
 					CustomerRefNo: "824225",
 					ProductCode:   "A02E",
-					FormDataValue: []ecobank.AirtimeTopupField{
+					FormDataValue: ecobank.FormDataArray{
 						{FieldName: "BEN_PHONE_NO", FieldValue: "2348034830707"},
 					},
 				}),
@@ -149,7 +149,7 @@ func main() {
 					CustomerName:  "Owen Kay",
 					CustomerRefNo: "824225",
 					ProductCode:   "AIRTELTIGO_MOBILEMONEY",
-					FormDataValue: []ecobank.MomoField{
+					FormDataValue: ecobank.FormDataArray{
 						{FieldName: "BEN_PHONE_NO", FieldValue: "0560000159"},
 					},
 				}),
@@ -157,77 +157,11 @@ func main() {
 				Currency: "GHS",
 				RateType: "spot",
 			},
-			{
-				RequestId:   "ByA1lUeHKK",
-				RequestType: ecobank.INTERBANKIA,
-				ParamList: ecobank.NewPaymentParams(ecobank.InterbankIAParams{
-					DestinationCountry:   "EKE",
-					DestinationBankCode:  "11000",
-					BeneficiaryAccountNo: "01108738185300",
-					BeneficiaryName:      "Harmony Blinks",
-					BeneficiaryPhone:     "233543837123",
-					Amount:               "100",
-					TransferCurrency:     "GHS",
-					TransferReason:       "vacation",
-					SettleCurrency:       "USD",
-				}),
-				Amount:   decimal.NewFromInt(100),
-				Currency: "GHS",
-				RateType: "spot",
-			},
-			{
-				RequestId:   "ByA1lUeHUU",
-				RequestType: ecobank.TOKENIA,
-				ParamList: ecobank.NewPaymentParams(ecobank.TokenIAParams{
-					DestAffiliate:          "EKE",
-					DestCrncy:              "KES",
-					DestinationAccount:     "01108738185300",
-					DestinationAccountName: "Owen",
-					ReceiveFirstName:       "Eugene",
-					ReceiveLastName:        "Eugene",
-					ReceiverPhoneNumber:    "233543837123",
-					ReceiveEmailAddress:    "test@gmail.com",
-					ReceiveIdType:          "23421221",
-					ReceiveIdNumber:        "23421221",
-					SourceAmount:           "100",
-					TestQuestion:           "GHS",
-					TestAnswer:             "vacation",
-					Narration:              "narration",
-					PurposeOfTransfer:      "purposeOfTransfer",
-					SendExternalRef:        "ByA1lUeHUU",
-				}),
-				Amount:   decimal.NewFromInt(100),
-				Currency: "GHS",
-				RateType: "spot",
-			},
-			{
-				RequestId:   "ByA1lUeHSS",
-				RequestType: ecobank.TOKENIA,
-				ParamList: ecobank.NewPaymentParams(ecobank.TokenIAParams{
-					DestAffiliate:          "EKE",
-					DestCrncy:              "KES",
-					DestinationAccount:     "01108738185300",
-					DestinationAccountName: "Owen",
-					ReceiveFirstName:       "Eugene",
-					ReceiveLastName:        "Eugene",
-					ReceiverPhoneNumber:    "233543837123",
-					ReceiveEmailAddress:    "test@gmail.com",
-					ReceiveIdType:          "23421221",
-					ReceiveIdNumber:        "23421221",
-					SourceAmount:           "100",
-					TestQuestion:           "GHS",
-					TestAnswer:             "vacation",
-					Narration:              "narration",
-					PurposeOfTransfer:      "purposeOfTransfer",
-					SendExternalRef:        "ByA1lUeHSS",
-				}),
-				Amount:   decimal.NewFromInt(100),
-				Currency: "GHS",
-				RateType: "spot",
-			},
 		},
 	}
 
+	// this is just to pass the test in the sandbox environment.
+	// you can omit this, and it will be automatically generated.
 	req.SetHash("398d4f285cc33e12f035da19fa9d954be35afaf66816531c4f1a1aedd3c6f132a85c62b23ca12d7b9a99bf5a84fc69b66738289a70e8f8115e90ffaa060f4026")
 
 	// make payment
--- payment_types.go
+++ payment_types.go
@@ -2,7 +2,10 @@ package ecobank
 
 import (
 	"encoding/json"
-	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/shopspring/decimal"
 )
 
 type PaymentType string
@@ -19,10 +22,13 @@ const (
 	MOMOIA       PaymentType = "MOMOIA"
 )
 
+// PaymentParamInterface defines an interface for payment parameters that can be serialized into JSON.
 type PaymentParamInterface interface {
 	json.Marshaler
 }
 
+// SupportedPaymentParamTypes is a type constraint for generics in PaymentParams.
+// It ensures that only predefined payment parameter structs can be used.
 type SupportedPaymentParamTypes interface {
 	DomesticTransferParams | TokenTransferParams | InterbankTransferParams |
 		BillPaymentParams | AirtimeTopupParams | MomoParams |
@@ -39,140 +45,201 @@ func NewPaymentParams[T SupportedPaymentParamTypes](param T) PaymentParamInterfa
 	return &PaymentParams[T]{param: param}
 }
 
-// MarshalJSON implements json.Marshaler interface
+// MarshalJSON implements the json.Marshaler interface for PaymentParams.
+// It serializes the struct fields into a JSON-encoded string that follows
+// a specific key-value format required by the ecobank API.
+//
+// **Format:**
+// The output is a JSON array of objects, where each object represents a field
+// with two properties:
+//   - "key": The JSON field name of the struct.
+//   - "value": The corresponding value from the struct.
+//
+// Example Output:
+// ```json
+// "[
+//
+//	{\"key\": \"billerCode\", \"value\": \"Pass_Bio_ECI\"},
+//	{\"key\": \"billRefNo\", \"value\": \"239729\"},
+//	{\"key\": \"customerName\", \"value\": \"Freeman Kay\"},
+//	{\"key\": \"formDataValue\", \"value\": \"[{\\\"fieldName\\\": \\\"LastName\\\", \\\"fieldValue\\\": \\\"Kojo\\\"}]\"}
+//
+// ]"
+// ```
+//
+// **Special Handling:**
+//   - The `formDataValue` field is itself a JSON-encoded array of objects, each
+//     containing a `fieldName` and `fieldValue`. This nested structure is stringified
+//     to maintain consistency with the external API's expected format.
 func (param *PaymentParams[T]) MarshalJSON() ([]byte, error) {
-	b, err := json.Marshal(param.param)
-	if err != nil {
-		return nil, err
+	val := reflect.ValueOf(param.param)
+	typ := reflect.TypeOf(param.param)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+		typ = typ.Elem()
 	}
 
+	var b strings.Builder
+	b.WriteString(`"[`)
+
+	for i := 0; i < val.NumField(); i++ {
+		fieldType := typ.Field(i)
+		fieldValue := val.Field(i)
+
+		// get the json tag of the field
+		jsonTag := fieldType.Tag.Get("json")
+		if jsonTag == "" || jsonTag == "-" {
+			continue
+		}
+
+		// now we need to set the {"key": "jsonTag", "value": "value"}
+		b.WriteString(`{\"key\": \"`)
+		b.WriteString(jsonTag)
+		b.WriteString(`\", \"value\": \"`)
+		// check if the field is FormDataArray
+		if jsonTag == "formDataValue" {
+			formData := fieldValue.Interface().(FormDataArray)
+			b.WriteString(`[`)
+			for i, fd := range formData {
+				b.WriteString(`{\\\"fieldName\\\": \\\"`)
+				b.WriteString(fd.FieldName)
+				b.WriteString(`\\\", \\\"fieldValue\\\": \\\"`)
+				b.WriteString(fd.FieldValue)
+				b.WriteString(`\\\"}`)
+				if i < len(formData)-1 {
+					b.WriteString(`,`)
+				}
+			}
+			b.WriteString(`]`)
+		} else {
+			b.WriteString(formatToStr(fieldValue.Interface()))
+
+		}
+		b.WriteString(`\"}`)
+		if i < val.NumField()-1 {
+			b.WriteString(`,`)
+		}
+	}
+
+	b.WriteString(`]"`)
+
 	// converted to a single quoted string
-	return []byte(fmt.Sprintf("%q", b)), nil
+	return []byte(b.String()), nil
 }
 
 // DomesticTransferParams represents the parameters for a domestic transfer.
 type DomesticTransferParams struct {
-	CreditAccountNo     string `json:"creditAccountNo"`
-	DebitAccountBranch  string `json:"debitAccountBranch"`
-	DebitAccountType    string `json:"debitAccountType"`
-	CreditAccountBranch string `json:"creditAccountBranch"`
-	CreditAccountType   string `json:"creditAccountType"`
-	Amount              string `json:"amount"`
-	Currency            string `json:"ccy"`
+	CreditAccountNo     string          `json:"creditAccountNo"`
+	DebitAccountBranch  string          `json:"debitAccountBranch"`
+	DebitAccountType    string          `json:"debitAccountType"`
+	CreditAccountBranch string          `json:"creditAccountBranch"`
+	CreditAccountType   string          `json:"creditAccountType"`
+	Amount              decimal.Decimal `json:"amount"`
+	Currency            string          `json:"ccy"`
 }
 
 // TokenTransferParams represents the parameters for a token transfer.
 type TokenTransferParams struct {
-	TransactionDescription string `json:"transactionDescription"`
-	SecretCode             string `json:"secretCode"`
-	SourceAccount          string `json:"sourceAccount"`
-	SourceAccountCurrency  string `json:"sourceAccountCurrency"`
-	SourceAccountType      string `json:"sourceAccountType"`
-	SenderName             string `json:"senderName"`
-	Currency               string `json:"ccy"`
-	SenderMobileNo         string `json:"senderMobileNo"`
-	Amount                 string `json:"amount"`
-	SenderID               string `json:"senderId"`
-	BeneficiaryName        string `json:"beneficiaryName"`
-	BeneficiaryMobileNo    string `json:"beneficiaryMobileNo"`
-	WithdrawalChannel      string `json:"withdrawalChannel"`
+	TransactionDescription string          `json:"transactionDescription"`
+	SecretCode             string          `json:"secretCode"`
+	SourceAccount          string          `json:"sourceAccount"`
+	SourceAccountCurrency  string          `json:"sourceAccountCurrency"`
+	SourceAccountType      string          `json:"sourceAccountType"`
+	SenderName             string          `json:"senderName"`
+	Currency               string          `json:"ccy"`
+	SenderMobileNo         string          `json:"senderMobileNo"`
+	Amount                 decimal.Decimal `json:"amount"`
+	SenderID               string          `json:"senderId"`
+	BeneficiaryName        string          `json:"beneficiaryName"`
+	BeneficiaryMobileNo    string          `json:"beneficiaryMobileNo"`
+	WithdrawalChannel      string          `json:"withdrawalChannel"`
 }
 
 // TokenIAParams represents the parameters for a token inter-affiliate transfer.
 type TokenIAParams struct {
-	DestAffiliate          string `json:"destAffiliate"`
-	DestCrncy              string `json:"destCrncy"`
-	DestinationAccount     string `json:"destinationAccount"`
-	DestinationAccountName string `json:"destinationAccountName"`
-	ReceiveFirstName       string `json:"receiveFirstName"`
-	ReceiveLastName        string `json:"receiveLastName"`
-	ReceiverPhoneNumber    string `json:"receiverPhoneNumber"`
-	ReceiveEmailAddress    string `json:"receiveEmailAddress"`
-	ReceiveIdType          string `json:"receiveIdType"`
-	ReceiveIdNumber        string `json:"receiveIdNumber"`
-	SourceAmount           string `json:"sourceAmount"`
-	TestQuestion           string `json:"testQuestion"`
-	TestAnswer             string `json:"testAnswer"`
-	Narration              string `json:"narration"`
-	PurposeOfTransfer      string `json:"purposeOfTransfer"`
-	SendExternalRef        string `json:"sendExternalRef"`
+	DestAffiliate          string          `json:"destAffiliate"`
+	DestCrncy              string          `json:"destCrncy"`
+	DestinationAccount     string          `json:"destinationAccount"`
+	DestinationAccountName string          `json:"destinationAccountName"`
+	ReceiveFirstName       string          `json:"receiveFirstName"`
+	ReceiveLastName        string          `json:"receiveLastName"`
+	ReceiverPhoneNumber    string          `json:"receiverPhoneNumber"`
+	ReceiveEmailAddress    string          `json:"receiveEmailAddress"`
+	ReceiveIdType          string          `json:"receiveIdType"`
+	ReceiveIdNumber        string          `json:"receiveIdNumber"`
+	SourceAmount           decimal.Decimal `json:"sourceAmount"`
+	TestQuestion           string          `json:"testQuestion"`
+	TestAnswer             string          `json:"testAnswer"`
+	Narration              string          `json:"narration"`
+	PurposeOfTransfer      string          `json:"purposeOfTransfer"`
+	SendExternalRef        string          `json:"sendExternalRef"`
 }
 
 // InterbankTransferParams represents the parameters for an interbank transfer.
 type InterbankTransferParams struct {
-	DestinationBankCode  string `json:"destinationBankCode"`
-	SenderName           string `json:"senderName"`
-	SenderAddress        string `json:"senderAddress"`
-	SenderPhone          string `json:"senderPhone"`
-	BeneficiaryAccountNo string `json:"beneficiaryAccountNo"`
-	BeneficiaryName      string `json:"beneficiaryName"`
-	BeneficiaryPhone     string `json:"beneficiaryPhone"`
-	TransferReferenceNo  string `json:"transferReferenceNo"`
-	Amount               string `json:"amount"`
-	Currency             string `json:"ccy"`
-	TransferType         string `json:"transferType"`
+	DestinationBankCode  string          `json:"destinationBankCode"`
+	SenderName           string          `json:"senderName"`
+	SenderAddress        string          `json:"senderAddress"`
+	SenderPhone          string          `json:"senderPhone"`
+	BeneficiaryAccountNo string          `json:"beneficiaryAccountNo"`
+	BeneficiaryName      string          `json:"beneficiaryName"`
+	BeneficiaryPhone     string          `json:"beneficiaryPhone"`
+	TransferReferenceNo  string          `json:"transferReferenceNo"`
+	Amount               decimal.Decimal `json:"amount"`
+	Currency             string          `json:"ccy"`
+	TransferType         string          `json:"transferType"`
 }
 
 // InterbankIAParams represents the parameters for an interbank inter-affiliate transfer.
 type InterbankIAParams struct {
-	DestinationCountry   string `json:"destinationCountry"`
-	DestinationBankCode  string `json:"destinationBankCode"`
-	BeneficiaryAccountNo string `json:"beneficiaryAccountNo"`
-	BeneficiaryName      string `json:"beneficiaryName"`
-	BeneficiaryPhone     string `json:"beneficiaryPhone"`
-	Amount               string `json:"amount"`
-	TransferCurrency     string `json:"transferCurrency"`
-	TransferReason       string `json:"transferReason"`
-	SettleCurrency       string `json:"settleCurrency"`
+	DestinationCountry   string          `json:"destinationCountry"`
+	DestinationBankCode  string          `json:"destinationBankCode"`
+	BeneficiaryAccountNo string          `json:"beneficiaryAccountNo"`
+	BeneficiaryName      string          `json:"beneficiaryName"`
+	BeneficiaryPhone     string          `json:"beneficiaryPhone"`
+	Amount               decimal.Decimal `json:"amount"`
+	TransferCurrency     string          `json:"transferCurrency"`
+	TransferReason       string          `json:"transferReason"`
+	SettleCurrency       string          `json:"settleCurrency"`
 }
 
 // BillPaymentParams represents the parameters for a bill payment.
 type BillPaymentParams struct {
-	BillerCode    string             `json:"billerCode"`
-	BillRefNo     string             `json:"billRefNo"`
-	CbaRefNo      string             `json:"cbaRefNo"`
-	CustomerName  string             `json:"customerName"`
-	CustomerRefNo string             `json:"customerRefNo"`
-	ProductCode   string             `json:"productCode"`
-	FormDataValue []BillPaymentField `json:"formDataValue"`
-}
-
-// BillPaymentField represents a field in the bill payment form.
-type BillPaymentField struct {
-	FieldName  string `json:"fieldName"`
-	FieldValue string `json:"fieldValue"`
+	BillerCode    string        `json:"billerCode"`
+	BillRefNo     string        `json:"billRefNo"`
+	CbaRefNo      string        `json:"cbaRefNo"`
+	CustomerName  string        `json:"customerName"`
+	CustomerRefNo string        `json:"customerRefNo"`
+	ProductCode   string        `json:"productCode"`
+	FormDataValue FormDataArray `json:"formDataValue"`
 }
 
 // AirtimeTopupParams represents the parameters for an airtime topup.
 type AirtimeTopupParams struct {
-	BillerCode    string              `json:"billerCode"`
-	BillRefNo     string              `json:"billRefNo"`
-	CbaRefNo      string              `json:"cbaRefNo"`
-	CustomerName  string              `json:"customerName"`
-	CustomerRefNo string              `json:"customerRefNo"`
-	ProductCode   string              `json:"productCode"`
-	FormDataValue []AirtimeTopupField `json:"formDataValue"`
-}
-
-// AirtimeTopupField represents a field in the airtime topup form.
-type AirtimeTopupField struct {
-	FieldName  string `json:"fieldName"`
-	FieldValue string `json:"fieldValue"`
+	BillerCode    string        `json:"billerCode"`
+	BillRefNo     string        `json:"billRefNo"`
+	CbaRefNo      string        `json:"cbaRefNo"`
+	CustomerName  string        `json:"customerName"`
+	CustomerRefNo string        `json:"customerRefNo"`
+	ProductCode   string        `json:"productCode"`
+	FormDataValue FormDataArray `json:"formDataValue"`
 }
 
 // MomoParams represents the parameters for a mobile money transfer.
 type MomoParams struct {
-	BillerCode    string      `json:"billerCode"`
-	BillRefNo     string      `json:"billRefNo"`
-	CbaRefNo      string      `json:"cbaRefNo"`
-	CustomerName  string      `json:"customerName"`
-	CustomerRefNo string      `json:"customerRefNo"`
-	ProductCode   string      `json:"productCode"`
-	FormDataValue []MomoField `json:"formDataValue"`
+	BillerCode    string        `json:"billerCode"`
+	BillRefNo     string        `json:"billRefNo"`
+	CbaRefNo      string        `json:"cbaRefNo"`
+	CustomerName  string        `json:"customerName"`
+	CustomerRefNo string        `json:"customerRefNo"`
+	ProductCode   string        `json:"productCode"`
+	FormDataValue FormDataArray `json:"formDataValue"`
 }
 
-// MomoField represents a field in the mobile money transfer form.
-type MomoField struct {
+type FormDataArray []FormData
+
+// FormData represents a fieldName and fieldValue pair.
+type FormData struct {
 	FieldName  string `json:"fieldName"`
 	FieldValue string `json:"fieldValue"`
 }
